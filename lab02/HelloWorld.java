36a853f54a70b61822558f5481ba92e4f717edb3
KarmenKei <tsogbayarurtnasan@gmail.com>
2025-02-07 16:03:22 +0800

    HelloWorld.java nemev

---
+++ lab02/HelloWorld.java
@@ -0,0 +1,13 @@
+package lab02;
+
+public class HelloWorld {
+
+    // This is a bad comment style.
+    public static void main(String[] args) {
+      
+      // Printing the Hello World message
+      String message="Hello, World!"; // Missing spaces around the assignment operator
+      System.out.println(message);
+    }
+    
+  }
