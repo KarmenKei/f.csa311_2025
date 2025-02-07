bce84121c4f00f10aee4c1f7ff66f546fb2c0195
KarmenKei <tsogbayarurtnasan@gmail.com>
2025-02-07 16:08:43 +0800

    HelloWorld.java comment Fixed

--- lab02/HelloWorld.java
+++ lab02/HelloWorld.java
@@ -2,11 +2,9 @@ package lab02;
 
 public class HelloWorld {
 
-    // This is a bad comment style.
     public static void main(String[] args) {
       
-      // Printing the Hello World message
-      String message="Hello, World!"; // Missing spaces around the assignment operator
+      String message="Hello, World!"; // Fixed
       System.out.println(message);
     }
     
